a6415206a75e28fb6a2210c2a86de980f4398a27
Matthew <matthewvandyke08@gmail.com>
2021-02-17 19:43:51 -0800

    Sign Out Added

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,6 @@
+import { useAuth } from '@/lib/auth';
 import { SunIcon } from '@chakra-ui/icons';
-import { Box, Button, Container, Flex, Heading, HStack, IconButton, Image, Link, SimpleGrid, Stack, Text, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, Heading, HStack, IconButton, Image, Link, SimpleGrid, Spacer, Stack, Text, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { useRouter } from 'next/router'
 
 const NavBar = () => {
@@ -9,6 +10,7 @@ const NavBar = () => {
     const btnbg = useColorModeValue("cyan.300", "cyan.400");
     const boxbg = useColorModeValue("gray.200", "gray.700");
     const router = useRouter()
+    const auth = useAuth();
 
 
     return (
@@ -30,11 +32,32 @@ const NavBar = () => {
                 <Image src="brain.svg" alt="Brain Logo" boxSize="60px" _hover={{ cursor: "pointer" }} onClick={() => router.push('/')} />
             </Stack>
             <Flex alignItems="center">
-                <IconButton aria-label="Search database" icon={<SunIcon />} bg={btnbg} size="lg" onClick={toggleColorMode}>
-                    Theme
-            </IconButton>
+                {auth?.user ? (
+                    <>
+                        <Button
+                            onClick={(e) => auth.signout()}
+                            variant="solid"
+                            backgroundColor={btnbg}
+                            size="lg"
+                            mr={4}
+                            height="50px"
+                            _hover={{ bg: "#A0AEC0" }}
+                        >
+                            Sign Out
+                        </Button>
+                        <IconButton aria-label="Search database" icon={<SunIcon />} bg={btnbg} size="lg" onClick={toggleColorMode}>
+                            Theme
+                        </IconButton>
+                    </>
+
+                ) : (
+                        <IconButton aria-label="Search database" icon={<SunIcon />} bg={btnbg} size="lg" onClick={toggleColorMode}>
+                            Theme
+                        </IconButton>
+
+                    )}
             </Flex>
-        </Flex>
+        </Flex >
     )
 }
 
